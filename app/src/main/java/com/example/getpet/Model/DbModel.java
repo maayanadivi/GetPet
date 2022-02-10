35c3830414f0e7f4197e05fa1ae1026ac8cf7cec
alice-aidlin <aliceai@ac.sce.ac.il>
2022-02-10 13:05:21 +0200

    db model added upload image to the storage of firebase

--- app/src/main/java/com/example/getpet/Model/DbModel.java
+++ app/src/main/java/com/example/getpet/Model/DbModel.java
@@ -157,8 +157,7 @@ public class DbModel {
                             petsList.add(p);
                         }
                     }
-                }else {
-
+                } else {
                 }
                 listener.onComplete(petsList);
             }
